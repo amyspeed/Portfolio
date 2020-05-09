519d92d34087d51c9ba82d9616d75705e6b2a888
Amy Speed-Henley <amyspeed@Amys-MBP.attlocal.net>
2020-05-09 13:50:53 -0500

    knit fixed gif

--- index.js
+++ index.js
@@ -24,31 +24,92 @@ function handleNav() {
 }
 
 
+const STORE = [
+    {
+        'image': './images/world.png',
+        'alt': 'COVID-19 App screenshot',
+        'title': 'COVID-19 Analytics',
+        'description': '',
+        'liveLink' : 'https://covid-19.amyspeed.dev',
+        'liveButtonId' : 'js-covid-live',
+        'codeButtonId' : 'js-covid-code',
+        'aboutButtonId' : 'js-covid-about',
+    },
+    {
+        'image': './images/IPA.png',
+        'alt': 'IPA app screenshot',
+        'title': '[ aI pi eI ] IPA Learning App',
+        'description': `A gateway to your immersive International Phonetic Alphabet learning experience!<br />
+                        React/Redux/HTML5/CSS3/JavaScript
+                        Node.js/Express/MongoDB/Mongoose
+                        Travis CI/mLab/Heroku`,
+        'liveLink' : 'https://pacific-basin-65264.herokuapp.com/',
+        'liveButtonId' : 'js-IPA-live',
+        'codeButtonId' : 'js-IPA-code',
+        'aboutButtonId' : 'js-IPA-about',
+    },
+    {
+        'image': './images/Ditties.png',
+        'alt': 'Ditties app screenshot',
+        'title': `Ditties | The Songwriter's App`,
+        'description': `Where users can create and track their amazing song ideas.<br />
+                        JavaScript/jQuery/HTML5/CSS3
+                        Node.js/Express/MongoDB/Mongoose
+                        Travis CI/mLab/Heroku`,
+        'liveLink' : 'https://cryptic-atoll-45082.herokuapp.com/',
+        'liveButtonId' : 'js-Ditties-live',
+        'codeButtonId' : 'js-Ditties-code',
+        'aboutButtonId' : 'js-Ditties-about',
+    },
+    {
+        'image': './images/EventsInTech.png',
+        'alt': 'Events in Tech app screenshot',
+        'title': 'Events In Tech',
+        'description': `A front-end discovery application. Where users can search a third-party API for live Science and Technology events in their area.<br />
+                        JavaScript/jQuery/HTML5/CSS3`,
+        'liveLink' : 'https://amyspeed.github.io/EventsInTech/',
+        'liveButtonId' : 'js-Events-live',
+        'codeButtonId' : 'js-Events-code',
+        'aboutButtonId' : 'js-Events-about',
+    },
+    {
+        'image': './images/VocalLit.png',
+        'alt': 'Vocal Lit app screenshot',
+        'title': 'Art Song and Opera Composers',
+        'description': `A Vocal Lit Quiz of Fun Facts!!!<br />
+                        Along with an identifying work of vocal music, this front-end quiz features bizarre facts about each composer.<br />
+                        JavaScript/jQuery/HTML5/CSS3`,
+        'liveLink' : 'https://amyspeed.github.io/VocalLit-QuizApp/',
+        'liveButtonId' : 'js-Lit-live',
+        'codeButtonId' : 'js-Lit-code',
+        'aboutButtonId' : 'js-Lit-about',
+    },
+]
+
 function appendProjects() {
+    for (let i = 0; i < STORE.length; i++) {
     $('.projects').append(
         `<div class="row">
             <div class="col-6">
                 <div class = "box">
-                    <img class = "screenshot" src="./images/IPA.png" alt = "landing page of IPA app"/>
+                    <img class = "screenshot" src="${STORE[i].image}" alt = "${STORE[i].alt}"/>
                 </div>
             </div>
             <div class="col-6">
                 <div class="box">
                 <div class = "info">
-                    <h4><a href = "https://pacific-basin-65264.herokuapp.com/" target = "_blank">[ aI pi eI ] IPA Learning App</a></h4>
-                    <p>A gateway to your immersive International Phonetic Alphabet learning experience!<br />
-                        React/Redux/HTML5/CSS3/JavaScript
-                        Node.js/Express/MongoDB/Mongoose
-                        Travis CI/mLab/Heroku</p>
-                    <button id = "js-IPA-live">live</button>
-                    <button id = "js-IPA-code">code</button>
-                    <button id = "js-IPA-about">about</button>
+                    <h4><a href = "${STORE[i].liveLink}" target = "_blank">${STORE[i].title}</a></h4>
+                    <p>${STORE[i].description}</p>
+                    <button id = "${STORE[i].liveButtonId}">live</button>
+                    <button id = "${STORE[i].codeButtonId}">code</button>
+                    <button id = "${STORE[i].aboutButtonId}">about</button>
                 </div>
                 </div>
             </div>
         </div>`
 
     );
+    }
 }
 
 handleNav();
